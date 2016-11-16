73a025637b417fddd5b3a75c0557dbc004b41456
torevb <torevb@hotmail.com>
2016-11-16 11:46:09 +0100

    shine og puss.

--- menu.c
+++ menu.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
-#include "solenoid.h"
 
+#include "solenoid.h"
 #include "OLED.h"
 #include "joy_ctrl.h"
 #include "CAN_driver.h"
@@ -23,6 +23,8 @@ node playgame_node;
 node highscore_node;
 node settings_node;
 node draw_node;
+node ingame_node;
+node calibrate_joy_node;
 
 node * current_node;
 
@@ -35,39 +37,44 @@ void menu_init(){
 	
 	root_node.name = "        > Main menu < \n";
 	root_node.parent = &root_node;
+	for (int i=0; i<7;i++){ root_node.children[i] = NULL; }
 	root_node.children[0] = &playgame_node;
 	root_node.children[1] = &highscore_node;
 	root_node.children[2] = &settings_node;
-	root_node.children[3]= &draw_node;
-	root_node.content_string = "Play game \nHighscores \nSettings\n";
+	root_node.children[3] = &draw_node;
+	root_node.content_string = "Play Ping-Pong game \nHighscores \nSettings \nDraw a doodle \n";
 
-	playgame_node.name = "Back    > Play game < \n";
+	playgame_node.name = "Back    > New game < \n";
 	playgame_node.parent = &root_node;
-	for (int i=0; i<7;i++){
-		playgame_node.children[i]=NULL; 
-	}
-	playgame_node.content_string = "new game \n1v1 \n1vsComputer \n";
+	for (int i=0; i<7;i++){ playgame_node.children[i] = NULL; }
+	playgame_node.children[0] = &ingame_node; 	//Holder kanskje uten denne ekstra noden?
+	playgame_node.content_string = "Play game \n";	
 
 	highscore_node.name = "Back     > Highscore < \n";
 	highscore_node.parent = &root_node;
-	for (int i=0; i<7;i++){
-		highscore_node.children[i]=NULL;
-	}
+	for (int i=0; i<7;i++){ highscore_node.children[i] = NULL; }
 	highscore_node.content_string = "Player1 score. \nPlayer2 score. \n";
 
 	settings_node.name = "Back     > Settings < \n";
 	settings_node.parent = &root_node;
-	for (int i=0; i<7;i++){
-		settings_node.children[i]=NULL;
-	}
-	settings_node.content_string = "calibrate_node \nvolume_node \n";
+	for (int i=0; i<7;i++){ settings_node.children[i] = NULL; }
+	settings_node.content_string = "Clear highscore \nCalibrate joystick \nTest SRAM \nCalibrate motor encoder \n";
 	
-	draw_node.name = "Drawing board ";
+	draw_node.name = "         > Drawing board < \n";
 	draw_node.parent = &root_node;
-	for (int i=0; i<7;i++){
-		draw_node.children[i]=NULL;
-	}
+	for (int i=0; i<7;i++){ draw_node.children[i] = NULL; }
 	//draw_node.content_string = draw_OLED();
+	
+	calibrate_joy_node.name = "         > Calibrate joystick < \n";
+	calibrate_joy_node.parent = &settings_node;
+	for (int i=0; i<7;i++){ calibrate_joy_node.children[i] = NULL; }
+	calibrate_joy_node.content_string = "Reset axis offset \nIncrease neutral zone \nDecrease neutral zone \n";
+	
+	ingame_node.name = "         > In-game < \n";
+	ingame_node.parent = &playgame_node;
+	for (int i=0; i<7;i++){ ingame_node.children[i] = NULL; }
+	ingame_node.content_string = "Current score: \n";
+	
 	
 	current_node = &playgame_node;//&root_node;
 	arrow_page = 0;
@@ -87,18 +94,18 @@ void menu_arrow(){
 
 /* Left slider button, INT0_vect, pin PD2. Right slider button, INT1_vect, pin PD3.*/
 ISR(INT0_vect){//, INT1_vect){
-	//if (current_node == &playgame_node){
+	if (current_node == &ingame_node){
 		CAN_struct solenoide_push;
 		solenoide_push.ID= SOLENOIDE_PUSH_ID;
 		solenoide_push.data[0]=0;
 		solenoide_push.length=1;
-		printf("Sending following message :\n ID: %i \n data: %i\n length %i",solenoide_push.ID,solenoide_push.data[0],solenoide_push.length);
+		if (DEBUG) { printf("Sending following message :\n ID: %i \n data: %i\n length %i",solenoide_push.ID,solenoide_push.data[0],solenoide_push.length); }
 		send_CAN_message(solenoide_push);
 		
 		
 		//push_solenoid();
-	//}
-	/*else{
+	}
+	else{	//This means we are in normal menu mode.
 		if (arrow_page <= 0){
 			//current_node = current_node->parent;
 		} else if (!(current_node->children[arrow_page - 1] == NULL)){
@@ -106,7 +113,7 @@ ISR(INT0_vect){//, INT1_vect){
 		}
 		arrow_page = 0;
 		menu_print();
-	}*/
+	}
 }
 
 /*Used for going backwards/upwards in menu*/
--- menu.h
+++ menu.h
@@ -1,27 +1,16 @@
 #pragma once
 
+#include <stdint.h>
 
 
 
+extern uint8_t highscore;
 
 void menu_init(void);
 
-
 void menu_print(void);
 
-
 void menu_arrow();
-
-
-
-
-
-
-
-
-
-
-
 
 
 
